dfa4e0353d47f343def5ab7556a7c58edf2767f0
Rahmadde <teukurahmad120@gmail.com>
2025-01-04 05:54:47 +0700

    Membuat proses edit (modul jurusan selesai)

---
+++ modul-jurusan/edit.php
@@ -0,0 +1,59 @@
+<?php
+#1. koneksi ke database
+include("../koneksi.php");
+
+#2. ambil id yang akan disunting
+$id = $_GET['id'];
+
+#3. mengambil semua record data berdasarkan id yang dipilih
+$ambil = "SELECT * FROM jurusans WHERE id='$id'";
+
+#4. menjalankan query
+$edit = mysqli_query($koneksi, $ambil);
+
+#5. memisahkan record data berdasarkan kolom/field
+if ($edit) {
+    $data = mysqli_fetch_array($edit);
+} else {
+    $data = null; // Handle the error as needed
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Data Jurusan</title>
+    <link rel="stylesheet" href="../css/bootstrap.css">
+    <link rel="stylesheet" href="../css/all.css">
+</head>
+<body>
+    <?php
+        include_once('../navbar.php');
+    ?>
+    <div class="container mt-5 col-8">
+        <div class="card">
+            <div class="card-header text-center">
+                <h3 class="float-start">
+                    Edit Data Jurusan
+                </h3>
+            </div>
+            <div class="card-body">
+                <form action="update.php" method="post">
+                    <input type="hidden" name="id" value="<?= isset($data['id']) ? $data['id'] : '' ?>">
+                    <div class="mb-3">
+                        <label for="exampleInputEmail1" class="form-label">Kode Jurusan</label>
+                        <input type="text" value="<?= isset($data['kode']) ? $data['kode'] : '' ?>" name="kode" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                    </div>
+                    <div class="mb-3">
+                        <label for="exampleInputPassword1" class="form-label">Nama Jurusan</label>
+                        <input type="text" value="<?= isset($data['jurusan']) ? $data['jurusan'] : '' ?>" name="jurusan" class="form-control" id="exampleInputPassword1">
+                    </div>
+                    
+                    <button type="submit" class="btn btn-primary">Update</button>
+    <script src="../js/bootstrap.js"></script>
+    <script src="../js/bootstrap.bundle.js"></script>
+    <script src="../js/all.js"></script>
+</body>
+</html>
